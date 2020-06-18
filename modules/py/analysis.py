8b634fc0f811879fc1288f150fcfe25032feed02
h-karim <karimh96@hotmail.com>
2020-06-18 15:31:46 -0400

    Add brief function documentation for Python

--- modules/py/analysis.py
+++ modules/py/analysis.py
@@ -1,3 +1,12 @@
+"""Slack messages VADER sentiment analyzer
+
+This script allows the user to perform certain manipulate Slack data received 
+as a JSON object:
+1- Clean the data from URLs
+2- Tokenize messages into individual sentences
+3- Perform a sentiment analysis on the sentences and aggregate the results
+The results are then printed as a JSON object for node to consume"""
+
 # -*- coding: utf-8 -*-
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk import sent_tokenize
@@ -8,15 +17,21 @@ import json
 analyzer = SentimentIntensityAnalyzer()
 
 
-def prepareSentences(text : str) -> list :
-    # remove URLs and other escaped hyperlinks between "< >"
-    # print(text)
+def prepareSentences(text: str) -> list:
+    """Gets a slack message and prepares it by removing URLS and other special
+    Slack identifiers, then tokenize message into sentences.
+
+    """
     text = re.sub(r'<(.*?)>', "", text)
     sentences = sent_tokenize(text)
     return sentences
 
 
-def analyzeIncoming(data : List) -> dict:
+def analyzeIncoming(data: List) -> dict:
+    """Perform a VADER sentiment analysis on individual
+    sentences and returns the results as a list of dictionaries
+
+    """
     results = []
 
     #print("data: ", data)
